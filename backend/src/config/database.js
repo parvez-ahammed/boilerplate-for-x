69a7d132ac61a222652756e664272b13eb5f49e6
piru72 <parvezdirom2000@gmail.com>
2024-05-28 02:20:09 +0600

    chore : added database related instructions

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -4,7 +4,7 @@ const { DynamoDBClient } = require('@aws-sdk/client-dynamodb');
 const client = new DynamoDBClient({
   region: 'us-west-2',
   endpoint: 'http://localhost:8000',
-  apiVersion: '2012-08-10'
+  apiVersion: '1'
 });
 
 module.exports = { client };
